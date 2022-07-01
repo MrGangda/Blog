d57cb59ecca35251599748a6c795eb535b48f52a
Kirill Alyoshin <alyoshinML@gmail.com>
2022-07-01 13:32:30 +0300

    'delete some components and add first logic'

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <button @click="addLike">Like</button>
+  <button @click="addDislike">Dislike</button>
+  <div>Кол-во лайков: {{ likes }} </div>
+  <div>Кол-во лайков: {{ dislikes }} </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 5,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1
+    },
+    addDislike() {
+    this.dislikes += 1
+  }
+  }
+  
+}
+
+</script>
+
+
+
+<style>
+
+</style>
